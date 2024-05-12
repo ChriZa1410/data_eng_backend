7cbd3e11bcd01a0563a4e6018103dd1bd2d30069
Christoph Z <christoph.zach@iu-study.org>
2024-05-12 21:37:15 +0200

    added influxdb settings

--- influxdb/create_influxdb.sh
+++ influxdb/create_influxdb.sh
@@ -8,6 +8,6 @@ DB_NAME=smartwatch_database
 # Send POST request to create schema
 #echo 'Waiting for server start...'
 #sleep 45
-echo 'Creating database $DB_NAME...'
+echo 'Creating database ' + $DB_NAME
 
 docker-compose exec influxdb influx -execute "CREATE DATABASE $DB_NAME"
